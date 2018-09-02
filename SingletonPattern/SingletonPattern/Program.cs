622244e02198a357da9a80ea00f9f547c850a4c9
tnayanam <nayatanuj@gmail.com>
2018-09-02 08:43:19 -0400

    two instance is getting created

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = new Singleton(); // suppose this was needed on employee class
+            fromEmployee.PrintDetails("from employee");
+            Singleton fromStudent = new Singleton(); // suppose this was needed in student class
+            fromStudent.PrintDetails("from student");
+            // not both created an instance of the class.
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        public static int count;
+
+        public Singleton()
+        {
+            count++;
+            Console.WriteLine($"Current count is {count}");
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
+// Output:
+//Current count is 1
+//from employee
+//Current count is 2
+//from student
+//Press any key to continue . . 
+// so we have two counts here, so two instance is created.
