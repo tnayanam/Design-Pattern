53ff2889b9ae0022834caa0f6dfd7c6a05cb52c4
tnayanam <nayatanuj@gmail.com>
2018-09-02 09:03:41 -0400

    Singleton Class: a basic one

--- SingletonPattern/SingletonPattern/Program.cs
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Singleton fromEmployee = new Singleton(); // suppose this was needed on employee class
+            Singleton fromEmployee = Singleton.GetInstance;// suppose this was needed on employee class
             fromEmployee.PrintDetails("from employee");
-            Singleton fromStudent = new Singleton(); // suppose this was needed in student class
+            Singleton fromStudent = Singleton.GetInstance; // suppose this was needed in student class
             fromStudent.PrintDetails("from student");
             // not both created an instance of the class.
         }
--- SingletonPattern/SingletonPattern/Singleton.cs
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -2,15 +2,26 @@
 
 namespace SingletonPattern
 {
-    class Singleton
+    public sealed class Singleton
     {
         public static int count;
+        private static Singleton singleton = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (singleton == null)
+                    singleton = new Singleton();
+                return singleton;
+            }
+        }
 
-        public Singleton()
+        private Singleton()
         {
             count++;
-            Console.WriteLine($"Current count is {count}");
+            Console.WriteLine($"count is {count}");
         }
+
         public void PrintDetails(string message)
         {
             Console.WriteLine(message);
@@ -19,9 +30,15 @@ namespace SingletonPattern
 }
 
 // Output:
-//Current count is 1
-//from employee
-//Current count is 2
-//from student
-//Press any key to continue . . 
+// Current count is 1
+// from employee
+// Current count is 2
+// from student
+// Press any key to continue . . 
 // so we have two counts here, so two instance is created.
+// OUTPUT:
+// count is 1
+// from employee
+// from student
+// Press any key to continue . . .// so now the count is one, even thpugh we called it from 
+// employee and student classes. // but it does not work well in multi threaded environment.
